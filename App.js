a5824eed9cf41238d0838a2d1347345adfcc17ba
Carrie Coxwell <carrie.coxwell@gmail.com>
2023-01-04 15:56:59 -0600

    button

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { React, useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [message, setMessage] = useState('')
+
+	return (
+		<View style={styles.container}>
+			<Button
+				onPress={() => setMessage('hi')}
+				title="Click me"></Button>
+			<Text>{message}</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	button: {
+		alignItems: 'center',
+		backgroundColor: '#DDDDDD',
+		padding: 10,
+		marginBottom: 10,
+	},
+})
